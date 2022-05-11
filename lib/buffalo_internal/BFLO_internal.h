c4085ca825229c44b0385caf75a2850184316262
tse503 <59828419+tse503@users.noreply.github.com>
2022-05-11 15:21:37 +0100

    Added more comments to BFLO_internal.h

--- lib/buffalo_internal/BFLO_internal.h
+++ lib/buffalo_internal/BFLO_internal.h
@@ -12,11 +12,13 @@
 
 #define PI 3.141592653589793
 
+/* Enumeration for potential errors */
 typedef enum {
     BFLO_MODULE_ALLOCATION_ERROR    = 0x00U, 
     BFLO_MODULE_ALLOCATION_SUCCESS  = 0x01U
 } BFLO_ERROR_t;
 
+/* Enumeration for dataflow input/output types */
 enum IO_type {BUFFER, CONTROL, TABLE};
 // TODO: enum for parameter type?
 
@@ -29,28 +31,32 @@ enum IO_type {BUFFER, CONTROL, TABLE};
 
 /*~~~~~ Structure Definitions ~~~~~*/
 
+/* Data type for a graph to store modules */
 typedef struct { // QUESTION: Separate array for parameter and operation modules? 
     uint32_t size;                              // Current count of modules connected in the graph                    
     struct module * modules[MAX_GRAPH_SIZE];    // Array to hold pointer-to-modules
 } graph_t;
 
+/* Data type for a module input */
 typedef struct {
     enum IO_type type;          // Description of input data type
     struct module * source;     // Pointer to source module that data is received from
     uint32_t index;             // Index of source module's output to receive data from
 } input_t;
 
+/* Data type for a module output */
 typedef struct {
     enum IO_type type;  // Description of input data type // TODO: Make this const?
     void * data;        // Pointer to memory where output data is stored  // QUESTION: Use union instead?
 } output_t;
 
-/*  */
+/* Data type for a module internal parameters */
 typedef struct {
     char name[MAX_NAME_LENGTH]; // Name of the parameter // TODO: Make this const?
     void * data;                // Pointer to memory where parameter data is stored
 } parameter_t;
 
+/* Data type for a dataflow module */
 typedef struct module { // QUESTION: Is this kind of forward-declaration sutiable for resolving circular dependencies between module_t and input_t?
     char name[MAX_NAME_LENGTH];         // String module name // TODO: Make this const?
     uint32_t status;                    // Holds boolean flags about the module's status
@@ -64,6 +70,7 @@ typedef struct module { // QUESTION: Is this kind of forward-declaration sutiabl
     graph_t * graph;                    // Pointer to graph that contains the module
 } module_t;
 
+/* Data type for a lookup table */
 typedef struct {
     char name[MAX_NAME_LENGTH];         // Name of the lookup table
     float samples[MAX_LUT_SIZE];        // Array of samples in the lookup table
